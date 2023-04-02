d4fa35e2cfbe7c7df4dda2e46fb0a0eef8e55bef
RuntimeRacer <49956579+RuntimeRacer@users.noreply.github.com>
2023-04-02 23:45:09 +0200

    Added simple run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RABBITMQ_USER=$1
+RABBITMQ_PASS=$2
+RABBITMQ_HOST=$3
+RABBITMQ_PORT=$4
+POLL_QUEUE=$5
+PUSH_QUEUE=$6
+KOBOLD_AI_HOST=$7
+
+until python3 rabbitmq.py -u "$RABBITMQ_USER" -p "$RABBITMQ_PASS" -rh "$RABBITMQ_HOST" -rp "$RABBITMQ_PORT" -pl "$POLL_QUEUE" -pu "$PUSH_QUEUE" -kh "$KOBOLD_AI_HOST"; do
+    echo "RabbitMQ worker crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
