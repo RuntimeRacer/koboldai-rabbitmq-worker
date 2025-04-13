4c7766994c3162430342d5f7670b07d59e027bc9
RuntimeRacer <sge2022@gmail.com>
2025-04-13 22:39:30 +0200

    Removed cache handling since it was cumbersome and added NACK handling for messages which couldn't be processed properly.

--- rabbitmq.py
+++ rabbitmq.py
@@ -5,7 +5,6 @@ import json
 import os
 import argparse
 import sys
-import threading
 import time
 
 import pika
@@ -20,23 +19,30 @@ def ack_message(ch, delivery_tag):
     if ch.is_open:
         ch.basic_ack(delivery_tag)
     else:
-        # Channel is already closed, so we can't ACK this message;
+        # Channel is already closed, so we can't ACK this message
+        pass
+
+
+def nack_message(ch, delivery_tag):
+    if ch.is_open:
+        ch.basic_nack(delivery_tag, requeue=True)
+    else:
+        # Channel is already closed, so we can't NACK this message
         pass
 
 
 class RabbitMQWorker:
     def __init__(
-            self,
-            rabbitmq_user: str,
-            rabbitmq_pass: str,
-            rabbitmq_host: str,
-            rabbitmq_port: int,
-            poll_channel: str,
-            push_channel: str,
-            inference_server_host: str,
-            cache_size: int = 1,
-            api_mode: str = "openai",  # either 'openai' for new format, or 'legacy' for old KoboldAI format
-            model_name: str = ''
+        self,
+        rabbitmq_user: str,
+        rabbitmq_pass: str,
+        rabbitmq_host: str,
+        rabbitmq_port: int,
+        poll_channel: str,
+        push_channel: str,
+        inference_server_host: str,
+        api_mode: str = "openai",  # either 'openai' for new format, or 'legacy' for old KoboldAI format
+        model_name: str = ''
     ):
         # Very simple validity checks
         if len(rabbitmq_user) == 0:
@@ -51,8 +57,6 @@ class RabbitMQWorker:
             raise RuntimeError("push_channel not set")
         if len(inference_server_host) == 0:
             raise RuntimeError("inference_server_host not set")
-        if cache_size < 0:
-            raise RuntimeError("invalid cache size")
         if api_mode not in ["openai", "legacy"]:
             raise RuntimeError(f"invalid api mode '{api_mode}'")
 
@@ -67,9 +71,6 @@ class RabbitMQWorker:
 
         # Handling params
         self.connection_active = False
-        self.cache_thread = None
-        self.cache_size = cache_size
-        self.cached_messages = []
         self.polling_connection = None
         self.polling_channel_ref = None
         self.pushing_connection = None
@@ -88,7 +89,7 @@ class RabbitMQWorker:
                     heartbeat=30
                 ))
                 self.polling_channel_ref = self.polling_connection.channel()
-                self.polling_channel_ref.basic_qos(prefetch_count=self.cache_size + 1)
+                self.polling_channel_ref.basic_qos(prefetch_count=1)
                 self.polling_channel_ref.queue_declare(queue=self.poll_channel, durable=True)
                 self.connection_active = True
                 logging.info("Successfully connected to RabbitMQ host")
@@ -97,10 +98,6 @@ class RabbitMQWorker:
                 logging.error("Retrying in 10 seconds...")
                 time.sleep(10)
                 continue
-
-            # Add cache processing thread and start it
-            self.cache_thread = CacheProcessingThread(worker=self)
-            self.cache_thread.start()
 
             # Start listening
             try:
@@ -119,7 +116,7 @@ class RabbitMQWorker:
         "MessageBody": str // Request Body 
         "MessageMetadata": str // Metadata for context to be shared between request sender and result receiver
     }
-    
+
     Returns the following structure in result:
     {
         "MessageID": str, // ID of the message defined by the sender, can be general UUID
@@ -133,203 +130,119 @@ class RabbitMQWorker:
         # Parse message
         logging.info("Received message from channel '{}': {}".format(self.poll_channel, body))
         data = json.loads(body)
-        message_id = data['MessageID'] if 'MessageID' in data else ''
-        message_body = data['MessageBody'] if 'MessageBody' in data else ''
-        message_metadata = data['MessageMetadata'] if 'MessageMetadata' in data else ''
+        message_id = data.get('MessageID', '')
+        message_body = data.get('MessageBody', '')
+        message_metadata = data.get('MessageMetadata', '')
 
         # Only process valid data
-        if len(message_id) == 0 or len(message_body) == 0:
+        if not message_id or not message_body:
             logging.warning("Message received was invalid. Skipping...")
+            # Acknowledge the invalid message to remove it from the queue
+            cb = functools.partial(ack_message, channel, method.delivery_tag)
+            self.polling_connection.add_callback_threadsafe(cb)
             return
 
-        # Add to cache - Before checking cache size
-        # This ensures we always process at least one message, even if cache is set to 0, because
-        # due to async nature oft the worker, it will always pull at least 1 message no matter what.
-        # Cache check will check cache size after adding the message, so this pulling connection is blocked until
-        # Cache is freed up again.
-        self.cached_messages.append({
-            'MessageID': message_id,
-            'MessageBody': message_body,
-            'MessageMetadata': message_metadata,
-            'ChannelRef': channel,
-            'DeliveryTag': method.delivery_tag
-        })
-        logging.debug("Added message with ID {} to cache".format(message_id))
-
-        # Check for Cache capacity and block if reached
-        if len(self.cached_messages) > self.cache_size:
-            # Send log line only once here, to avoid logspam
-            logging.debug("Cache is full, waiting for clearance...".format(message_id))
-            # Wait until cache is cleared
-            while len(self.cached_messages) > self.cache_size:
-                time.sleep(0.1)
-
-    def process_cached_messages(self):
-        while len(self.cached_messages) > 0:
-            # Get first message from cache
-            message = self.cached_messages[0]
-            message_body = message['MessageBody']
-            # Get RabbitMQ related data
-            channel = message['ChannelRef']
-            delivery_tag = message['DeliveryTag']
-
-            # Send Request to target inference server
-            result_json = {}
+        # Send Request to target inference server
+        result_json = {}
+        try:
             if self.api_mode == "openai":
                 # Add model name if defined
-                if len(self.model_name) > 0:
+                if self.model_name:
                     message_body['model'] = self.model_name
 
                 headers = {
                     "Content-Type": "application/json",
                 }
                 url = self.inference_server_host + "/v1/completions"
-
-                try:
-                    result = requests.post(url=url, headers=headers, json=message_body, timeout=30)
-                except Exception as e:
-                    logging.error("Inference server was unable to process message: {}".format(str(e)))
-                    logging.error("Retrying in 10 seconds...")
-                    time.sleep(10)
-                    continue
-
+                result = requests.post(url=url, headers=headers, json=message_body, timeout=30)
                 # Build Result
-                try:
-                    result = {
-                        "MessageID": message['MessageID'],
-                        "MessageBody": message['MessageBody'],
-                        "MessageMetadata": message['MessageMetadata'],
-                        "ResultStatus": result.status_code,
-                        "ResultBody": result.json(),
-                    }
-                    result_json = json.dumps(result)
-                except Exception as e:
-                    logging.error("failed to decode result from inference server: {}".format(str(e)))
-                    logging.error("Retrying in 10 seconds...")
-                    time.sleep(10)
-                    continue
-
+                result_data = {
+                    "MessageID": message_id,
+                    "MessageBody": message_body,
+                    "MessageMetadata": message_metadata,
+                    "ResultStatus": result.status_code,
+                    "ResultBody": result.json(),
+                }
+                result_json = json.dumps(result_data)
             elif self.api_mode == "legacy":
                 headers = {
                     "Content-Type": "application/json",
                 }
                 url = self.inference_server_host + "/api/v1/generate"
-                try:
-                    result = requests.post(url=url, headers=headers, json=message_body, timeout=30)
-                except Exception as e:
-                    logging.error("Inference server was unable to process message: {}".format(str(e)))
-                    logging.error("Retrying in 10 seconds...")
-                    time.sleep(10)
-                    continue
-
+                result = requests.post(url=url, headers=headers, json=message_body, timeout=30)
                 # Build Result
-                try:
-                    result = {
-                        "MessageID": message['MessageID'],
-                        "MessageMetadata": message['MessageMetadata'],
-                        "ResultStatus": result.status_code,
-                        "ResultBody": result.text,
-                    }
-                    result_json = json.dumps(result)
-                except Exception as e:
-                    logging.error("failed to decode result from inference server: {}".format(str(e)))
-                    logging.error("Retrying in 10 seconds...")
-                    time.sleep(10)
-                    continue
-
-            # Remove message from cache, AFTER being processed
-            self.cached_messages.pop(0)
-            # Send ACK to tasks channel
-            cb = functools.partial(ack_message, channel, delivery_tag)
+                result_data = {
+                    "MessageID": message_id,
+                    "MessageMetadata": message_metadata,
+                    "ResultStatus": result.status_code,
+                    "ResultBody": result.text,
+                }
+                result_json = json.dumps(result_data)
+        except Exception as e:
+            logging.error("An error occurred during message processing: {}".format(str(e)))
+            # Send negative acknowledgment to requeue the message
+            cb = functools.partial(nack_message, channel, method.delivery_tag)
             self.polling_connection.add_callback_threadsafe(cb)
+            return
 
-            # Publish to result queue
-            logging.info("Processing for message ID '{0}' completed. Sending Result: {1}".format(message['MessageID'], result_json))
-            result_sent = False
-            try:
-                self.pushing_connection = pika.BlockingConnection(pika.ConnectionParameters(
-                    host=self.rabbitmq_host,
-                    port=self.rabbitmq_port,
-                    credentials=pika.credentials.PlainCredentials(username=self.rabbitmq_user, password=self.rabbitmq_pass),
-                    heartbeat=30
-                ))
-                self.pushing_channel_ref = self.pushing_connection.channel()
-                self.pushing_channel_ref.queue_declare(queue=self.push_channel, durable=True)
-                self.pushing_channel_ref.basic_publish(exchange='', routing_key=self.push_channel, body=result_json)
-                result_sent = True
-                self.pushing_channel_ref.close()
-                self.pushing_connection.close()
-            except Exception as e:
-                if not result_sent:
-                    logging.error("Failed to send result: {0}".format(str(e)))
-                else:
-                    logging.warning("Exception after sending result: {0}".format(str(e)))
+        # Publish to result queue
+        logging.info("Processing for message ID '{0}' completed. Sending Result: {1}".format(message_id, result_json))
+        try:
+            self.pushing_connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host=self.rabbitmq_host,
+                port=self.rabbitmq_port,
+                credentials=pika.credentials.PlainCredentials(username=self.rabbitmq_user, password=self.rabbitmq_pass),
+                heartbeat=30
+            ))
+            self.pushing_channel_ref = self.pushing_connection.channel()
+            self.pushing_channel_ref.queue_declare(queue=self.push_channel, durable=True)
+            self.pushing_channel_ref.basic_publish(exchange='', routing_key=self.push_channel, body=result_json)
+            self.pushing_channel_ref.close()
+            self.pushing_connection.close()
+            logging.info("Result for message ID '{0}' was sent successfully!".format(message_id))
+        except Exception as e:
+            logging.error("Failed to send result: {0}".format(str(e)))
+            # Send a negative acknowledgment to requeue the message
+            cb = functools.partial(nack_message, channel, method.delivery_tag)
+            self.polling_connection.add_callback_threadsafe(cb)
+            # Shutdown connections & processing after exception
+            self.shutdown()
+            return
 
-                # Shutdown connections & processing after exception
-                self.shutdown()
-
-            if result_sent:
-                logging.info("Result for message ID '{0}' was sent successfully!".format(message['MessageID']))
+        # Acknowledge the message after successful processing
+        cb = functools.partial(ack_message, channel, method.delivery_tag)
+        self.polling_connection.add_callback_threadsafe(cb)
 
     def shutdown(self):
-        if self.cache_thread is not None:
-            self.cache_thread.active = False
-            self.cached_messages.clear()
-            self.cache_thread.join()
-
         if self.pushing_channel_ref is not None:
             try:
                 self.pushing_channel_ref.close()
             except Exception as e:
-                logging.warning("failed to shutdown pushing channel gracefully: {0}".format(str(e)))
+                logging.warning("Failed to shutdown pushing channel gracefully: {0}".format(str(e)))
 
         if self.pushing_connection is not None:
             try:
                 self.pushing_connection.close()
             except Exception as e:
-                logging.warning("failed to shutdown pushing connection gracefully: {0}".format(str(e)))
+                logging.warning("Failed to shutdown pushing connection gracefully: {0}".format(str(e)))
 
         if self.polling_channel_ref is not None:
             try:
                 self.polling_channel_ref.stop_consuming()
                 self.polling_channel_ref.close()
             except Exception as e:
-                logging.warning("failed to shutdown polling channel gracefully: {0}".format(str(e)))
+                logging.warning("Failed to shutdown polling channel gracefully: {0}".format(str(e)))
 
         if self.polling_connection is not None:
             try:
                 self.polling_connection.close()
             except Exception as e:
-                logging.warning("failed to shutdown polling connection gracefully: {0}".format(str(e)))
+                logging.warning("Failed to shutdown polling connection gracefully: {0}".format(str(e)))
 
         self.connection_active = False
 
 
-class CacheProcessingThread(threading.Thread):
-
-    def __init__(self, worker: RabbitMQWorker):
-        # execute the base constructor
-        threading.Thread.__init__(self)
-        # store the reference
-        self.worker = worker
-        # Internal vars
-        self.active = False
-
-    def run(self):
-        if self.worker is not None:
-            logging.info("Starting Cache Processing Thread")
-            self.active = True
-            while self.active:
-                self.worker.process_cached_messages()
-                # Sleep if not processing anything
-                time.sleep(0.01)
-        else:
-            raise RuntimeError("RabbitMQ Worker not initialized")
-
-
 if __name__ == "__main__":
-    global _polling_connection, _polling_channel, _pushing_connection
-
     # Init logger
     Formatter.converter = time.gmtime
     logging.basicConfig(
@@ -349,14 +262,11 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--rabbitmq_pass", type=str, help="password to establish broker connection")
     parser.add_argument("-rh", "--rabbitmq_host", type=str, help="host of the rabbitmq server")
     parser.add_argument("-rp", "--rabbitmq_port", type=int, default=5672, help="port of the rabbitmq server")
-    parser.add_argument("-pl", "--poll_channel", type=str, help="name if the rabbitmq channel to subscribe to")
-    parser.add_argument("-pu", "--push_channel", type=str, help="name if the rabbitmq channel to push results to")
+    parser.add_argument("-pl", "--poll_channel", type=str, help="name of the rabbitmq channel to subscribe to")
+    parser.add_argument("-pu", "--push_channel", type=str, help="name of the rabbitmq channel to push results to")
 
     # KoboldAI Parameters
     parser.add_argument("-ih", "--inference_server_host", type=str, help="host+port of the inference server")
-
-    # Worker Parameters
-    parser.add_argument("-cs", "--cache_size", type=int, default=1, help="amount of messages to cache while processing")
 
     # Backend Parameters
     parser.add_argument("-m", "--model_name", type=str, default='', help="explicit name of the model, required by some backends")
--- run.sh
+++ run.sh
@@ -7,10 +7,9 @@ RABBITMQ_PORT=$4
 POLL_QUEUE=$5
 PUSH_QUEUE=$6
 INFERENCE_SERVER_HOST=$7
-CACHE_SIZE=$8
-MODEL_NAME=$9
+MODEL_NAME=$8
 
-until python3 rabbitmq.py -u "$RABBITMQ_USER" -p "$RABBITMQ_PASS" -rh "$RABBITMQ_HOST" -rp "$RABBITMQ_PORT" -pl "$POLL_QUEUE" -pu "$PUSH_QUEUE" -ih "$INFERENCE_SERVER_HOST" -cs "$CACHE_SIZE" -m "$MODEL_NAME"; do
+until python3 rabbitmq.py -u "$RABBITMQ_USER" -p "$RABBITMQ_PASS" -rh "$RABBITMQ_HOST" -rp "$RABBITMQ_PORT" -pl "$POLL_QUEUE" -pu "$PUSH_QUEUE" -ih "$INFERENCE_SERVER_HOST" -m "$MODEL_NAME"; do
     timestamp=$(date -u +"[%Y-%m-%d %H:%M:%S %z]")
     echo "${timestamp} RabbitMQ worker crashed with exit code $?.  Respawning.." >&2
     sleep 1
